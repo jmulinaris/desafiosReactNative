1f949f776d94ae3108033aa3d557f6028b7a801e
Julieta <juli.mulinaris@gmail.com>
2023-08-09 21:40:26 -0300

    Finalizamos la configuración de la location, con guardado de la dirección en los datos del usuario (Firebase)

--- desafio6/src/screens/address/index.jsx
+++ desafio6/src/screens/address/index.jsx
@@ -1,19 +1,48 @@
-import { useState } from 'react';
-import { View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { View, Button } from 'react-native';
+import { useSelector } from 'react-redux';
 
 import styles from './styles';
 import { LocationSelector } from '../../components';
+import { GOOGLE_API_KEY, URL_BASE_GEOCODING } from '../../constants/maps';
+import { useUpdateAddressMutation } from '../../store/settings/api';
 
-const Address = () => {
+const Address = ({ navigation }) => {
+  const localId = useSelector((state) => state.auth.user.localId);
   const [location, setLocation] = useState(null);
+  const [address, setAddress] = useState(null);
+  const [updateAddress] = useUpdateAddressMutation();
 
-  const onLocation = () => {
-    setLocation(location);
+  const onLocation = async ({ lat, lng }) => {
+    setLocation({ lat, lng });
   };
+
+  const onHandlerUpdateLocation = () => {
+    updateAddress({ localId, address, location });
+    navigation.navigate('Settings');
+  };
+
+  useEffect(() => {
+    if (location) {
+      const getGeocoding = async () => {
+        const response = await fetch(
+          `${URL_BASE_GEOCODING}/json?latlng=${location.lat},${location.lng}&key=${GOOGLE_API_KEY}`
+        );
+        const data = await response.json();
+        if (!data.results) {
+          throw new Error('Something went wrong!');
+        }
+        const address = data.results[0].formatted_address;
+        setAddress(address);
+      };
+      getGeocoding();
+    }
+  }, [location]);
 
   return (
     <View style={styles.container}>
       <LocationSelector onLocation={onLocation} />
+      <Button title="Confirm" onPress={onHandlerUpdateLocation} />
     </View>
   );
 };
